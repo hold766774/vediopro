cbe10e2fec1a061476df52669851e6c249a116c9
hold766774 <1106246044@qq.com>
2017-06-23 13:38:10 +0800

    45视频上传准备 vue.js+七牛JSSDK集成(1)排坑
    1 下载pluload插件，复制，引入js文件
    2 配置相关参数，点击按钮id,父类id,视频路径等
    3 从后端获取token，其中使用上传策略
    4 设置文件过滤

--- src/jtthink/member-index.js
+++ src/jtthink/member-index.js
@@ -8,9 +8,12 @@ import VueResource from "vue-resource";
 import VueRouter from "vue-router";
 import "./../css/common.css";
 import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-default/icon.css'
 import navbar from './../components/member/navbar.vue';
 import {routerConfig} from  "./../config/RouterConfig"
 
+//require("qiniu-js-sdk/dist/qiniu.js");//引入js因为没有遵循commonjs ,所有之鞥呢这样引入
+require("./qiniu-js-sdk/dist/qiniu");
 import global from "./global";
 Vue.use(Vuex)
 Vue.use(ElementUI)
